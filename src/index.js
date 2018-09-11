2f5d27924bdedf674bde18700bf20eb9851b6e41
EvilaMany <lineman98@yandex.ru>
2018-09-11 20:53:03 +0300

    fifth task solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+	if(currency > 10000)
+		return {error:"You are rich, my friend! We don't have so much coins for exchange"}
+
+
+    var rates = {
+    	H : 50,
+    	Q : 25,
+    	D : 10,
+    	N : 5,
+    	P : 1
+    }
+    var result = {}
+
+
+    for(var rateName in rates)
+    {
+    	var ExAmount = Math.floor(currency / rates[rateName])
+    	if(ExAmount > 0)
+    	{
+    		result[rateName] = ExAmount;
+    		currency = currency % rates[rateName];
+    	}
+    }
+    return result;
 }
